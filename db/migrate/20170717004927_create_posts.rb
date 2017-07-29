cfdbdb765c4b83dca2797e71c964c56eb6eda6a5
menna <mennaxmohiex@gmail.com>
2017-07-29 12:33:52 -0300

    Changing user_id to integer in the Post model

--- db/migrate/20170717004927_create_posts.rb
+++ db/migrate/20170717004927_create_posts.rb
@@ -2,7 +2,7 @@ class CreatePosts < ActiveRecord::Migration[5.1]
   def change
     create_table :posts do |t|
       t.text :body
-      t.string :user_id
+      t.integer :user_id
 
       t.timestamps
     end
